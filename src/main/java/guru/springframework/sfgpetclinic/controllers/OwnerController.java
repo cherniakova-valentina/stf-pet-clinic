d472d075c17e9fb4a1ee24ae6129d46246bd2e88
v.cherniakov <valushechka1996@gmail.com>
2020-11-13 19:46:10 +0200

    Fixed broken links. Closes #45

--- src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -20,6 +20,11 @@ public class OwnerController {
 
         model.addAttribute("owners", ownerService.findAll());
 
-        return "owners/index.html";
+        return "owners/index";
+    }
+
+    @RequestMapping({"/find"})
+    public String findOwners() {
+        return "notimplemented";
     }
 }
